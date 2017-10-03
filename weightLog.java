a510cca4d39ecddd80708da89e6595fe9c8d01e0
j-liang <johnny.son@live.ca>
2017-10-03 14:04:16 -0400

    Added code to my local branch

---
+++ weightLog.java
@@ -0,0 +1,17 @@
+package arj.fittrack;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by John on 2017-10-03.
+ */
+
+public class weightLog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
