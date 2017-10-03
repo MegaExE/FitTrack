28b842ee5ee66a43d54e50cab274e4ff846b9b3b
RaphaelNajera <31861631+RaphaelNajera@users.noreply.github.com>
2017-10-03 14:28:02 -0400

    Added Weight.java

---
+++ Weight.java
@@ -0,0 +1,23 @@
+package arj.fittrack;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Owner on 10/2/2017.
+ */
+
+public class Weight extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weight);
+        //Add a upbutton to allow the user to go back to main screen
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
+
+
+    }
+}
