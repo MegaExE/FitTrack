c675eab6b559ba2a0ea99d148394ec28dcf68fe6
RaphaelNajera <31861631+RaphaelNajera@users.noreply.github.com>
2017-10-21 20:59:04 -0400

    Updated Main.java

--- Main.java
+++ Main.java
@@ -1,29 +1,35 @@
 package arj.fittrack;
 
+/**
+ *  Team Name: ARJ
+ */
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
-
-/**
-*Raphael Najera
-*ARJ
-*/
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Main extends AppCompatActivity {
-
+    final Context context = this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
-
-        final ImageButton weight = (ImageButton) findViewById(R.id.weight);
-        weight.setOnClickListener(new View.OnClickListener() {
+        //Weight button
+        //Open to the Weight screen
+        final ImageButton weightbutton = (ImageButton) findViewById(R.id.weight);
+        weightbutton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
                 Intent intent = new Intent(Main.this, Weight.class);
@@ -31,15 +37,35 @@ public class Main extends AppCompatActivity {
             }
         });
 
-
-        final ImageButton notepad = (ImageButton) findViewById(R.id.notepad);
-        notepad.setOnClickListener(new View.OnClickListener() {
+        //Notepad button
+        //Open to the Notepad screen
+        final ImageButton notepadbutton = (ImageButton) findViewById(R.id.notepad);
+        notepadbutton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
                 Intent intent = new Intent(Main.this, Notepad.class);
                 startActivity(intent);
             }
         });
+
+        //Set goals
+        //Open the goals screen
+        final Button setgoals = (Button) findViewById(R.id.setgoals);
+        setgoals.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(Main.this, Goals.class);
+
+
+
+                startActivity(intent);
+            }
+        });
+
+        //Display the current goals
+        //Using the database?
+        final TextView goal = (TextView) findViewById(R.id.viewgoal);
+        goal.setText("All goals are completed");
+       // Number.setText(b.getCharSequence("number"));
 
 
     }
@@ -60,12 +86,48 @@ public class Main extends AppCompatActivity {
         switch (item.getItemId())
         {
             case R.id.Help:
-
-
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+                // set title
+                alertDialogBuilder.setTitle("Help");
+                // set dialog message
+                alertDialogBuilder
+                        .setMessage("Help Message:")
+                        .setCancelable(false)
+                        .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // if this button is clicked, just close
+                                // the dialog box and do nothing
+                                dialog.cancel();
+                                //display in short period of time
+                                Toast.makeText(getApplicationContext(), "Ok is clicked",
+                                        Toast.LENGTH_LONG).show();
+                            }
+                        });
+                // create alert dialog
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                // show it
+                alertDialog.show();
                 break;
             case R.id.About:
-
-
+                AlertDialog.Builder alertDialogBuilder2 = new AlertDialog.Builder(context);
+                // set title
+                alertDialogBuilder2.setTitle("About");
+                // set dialog message
+                alertDialogBuilder2
+                        .setMessage("About Message:  \n Group: Johnson, Raphael, Adrian \n Group Name: ARJ \n App name: FitTrack \n ," +
+                                "GitHub: link \n Health App.")
+                        .setCancelable(false)
+                        .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // if this button is clicked, just close
+                                // the dialog box and do nothing
+                                dialog.cancel();
+                            }
+                        });
+                // create alert dialog
+                AlertDialog alertDialog2 = alertDialogBuilder2.create();
+                // show it
+                alertDialog2.show();
                 break;
             case R.id.Setting:
 
