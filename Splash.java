78da2a77455e685a888d1ec44943f4ceaab0b047
j-liang <31778227+j-liang@users.noreply.github.com>
2017-12-05 18:38:04 -0500

    updated Splash.java
    minor edits to splash where we enlarged the icon and reduced the splash time

--- Splash.java
+++ Splash.java
@@ -1,20 +1,35 @@
 package arj.fittrack;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.EditText;
 
 /**
  * Created by John on 2017-10-16.
  */
 
-public class Splash extends AppCompatActivity{
-
+public class Splash extends AppCompatActivity {
+    //A delay time to show the icon for 5 seconds (loading screen)
+    final int SPLASH_TIME = 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.weightactivity);
-        //Add a upbutton to allow the user to go back to main screen
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setContentView(R.layout.splashactivity);
+
+        Thread thread = new Thread(){
+            @Override
+            public void run(){
+                try {
+                    sleep(SPLASH_TIME);
+                    Intent intent = new Intent(getApplicationContext(), Main.class);
+                    startActivity(intent);
+                    finish();
+                }
+                catch (InterruptedException ex){
+                    ex.printStackTrace();
+                }
+            }
+        };
+        thread.start();
     }
 }
