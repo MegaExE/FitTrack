15ed429de4a37107fd18adce5f76840290b83459
RaphaelNajera <31861631+RaphaelNajera@users.noreply.github.com>
2017-12-12 04:05:02 -0500

    Updated Goal.java
    Added delete goal button. User can now delete the goals.

--- Goal.java
+++ Goal.java
@@ -1,10 +1,11 @@
 package arj.fittrack;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -20,91 +21,180 @@ import java.util.Arrays;
  *  Adrian Caprini N01115682, Raphael Najera N01104031, Johnson Liang N01129137
  */
 
-public class Goal extends AppCompatActivity {
+public class Goal extends Fragment {
+    private static final String PREFS_NAME = "prefs";
+    private static final String PREF_DARK_THEME = "dark_theme";
 
     //Declare ArrayList, ArrayAdapter and EditText
     static ArrayList<String> arrayList;
     static ArrayAdapter<String> adapter;
     EditText input;
 
+    //create an ArrayList object to store selected items
+    ArrayList<String> selectedItems;
+
+
+    //Declare Database
+    myDbAdapter helper;
+
+    //Declared the Context for Dialog
+    final Goal context = this;
+
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+
+        final View view = inflater.inflate(R.layout.goalactivity, container, false);
+
+        /*SharedPreferences preferences = getActivity().getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        boolean useDarkTheme = preferences.getBoolean(PREF_DARK_THEME, false);
+
+        if(useDarkTheme) {
+            getActivity().setTheme(R.style.AppTheme_Dark_NoActionBar);
+        }*/
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.goalactivity);
+        //getActivity().setContentView(R.layout.goal);
+        //  Toolbar toolbar = (Toolbar) getActivity().findViewById(R.id.toolbar);
+        //((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+        //Add a upbutton to allow the user to go back
+        // ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //Initialize the DatabaseHelper
+        helper = new myDbAdapter((getActivity().getApplicationContext()));
 
         //Creates the ListView
-        ListView listView = (ListView) findViewById(R.id.listv);
+        ListView listView = (ListView) view.findViewById(R.id.listv);
 
         //Stores the user's goal
         String[] goals = {};
+
+
+
         //Create an ArrayList object to store the user's goal
         arrayList = new ArrayList<>(Arrays.asList(goals));
         //adapter=new ArrayAdapter<String>(this,R.layout.list_item,R.id.txtitem,android.R.layout.simple_list_item_multiple_choice,arrayList);
         //Supply data items to Checkbox Listview
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, arrayList);
+        adapter = new ArrayAdapter<String>(getActivity().getApplicationContext(), android.R.layout.simple_list_item_multiple_choice, arrayList);
 
         //set multiple selection mode
         listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
 
         //Testing
         //arrayList.add("Test");
+
         listView.setAdapter(adapter);
+
+        //Gets the user's goals from the database
+        String data = helper.getData_Goals();
+        String[] test = data.split("\n");
+        //Displays the user's goals
+        for(String savegoals : test) {
+            arrayList.add(savegoals);
+        }
+
         //User's input for adding the Goals
-        input = (EditText) findViewById(R.id.editText);
+        input = (EditText) view.findViewById(R.id.editText);
+
+        selectedItems = new ArrayList<String>();
+
+
 
         //set OnItemClickListener
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItem = ((TextView) view).getText().toString();
+                if(selectedItems.contains(selectedItem))
+                    selectedItems.remove(selectedItem); //remove deselected item from the list of selected items
+                else
+                    selectedItems.add(selectedItem); //add selected item to the list of selected items
 
             }
         });
 
 
         //This button will set the user's Goals
-        final Button setgoals = (Button) findViewById(R.id.setgoal);
+        final Button setgoals = (Button) view.findViewById(R.id.setgoal);
         setgoals.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
                 TextView tv1;
                 EditText input;
-                input = (EditText) findViewById(R.id.editText);
+                input = (EditText) view.findViewById(R.id.editText);
 
-                tv1 = (TextView) findViewById(R.id.textView2);
-                tv1.setText(input.getText().toString());
+                String goals = input.getText().toString();
 
-                //Add the user's goal to the array and display the goal in the ListView
-                arrayList.add(input.getText().toString());
-                adapter.notifyDataSetChanged();
+                //Check if the EditText is empty or not
+                if(goals.isEmpty()){
+                    Message.message(getActivity().getApplicationContext(),"Enter a goal");
+                }else {
+
+                   // tv1 = (TextView) getActivity().findViewById(R.id.textView2);
+                    //tv1.setText(input.getText().toString());
+
+                    //Add the user's goal to the array and display the goal in the ListView
+                    arrayList.add(input.getText().toString());
+                    adapter.notifyDataSetChanged();
+
+
+                    //Add the user's goal to the database
+                    long id = helper.insertData_Goal(goals);
+                    if(id<0)
+                    {
+                        Message.message(getActivity().getApplicationContext(),"Insertion Unsuccessful");
+                        input.setText("");
+
+                    } else
+                    {
+                        Message.message(getActivity().getApplicationContext(),"Insertion Successful");
+                        input.setText("");
+                    }
+
+                }
             }
         });
 
-        //Floating Action Button will go to the Challenges Screen
-        FloatingActionButton challenges = (FloatingActionButton) findViewById(R.id.challenges);
-        challenges.setOnClickListener(new View.OnClickListener() {
-            @Override
+
+        //This button will delete the user's Goals
+        final Button deletegoal = (Button) view.findViewById(R.id.deletegoal);
+        deletegoal.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
-                //Goes to the Challenges Screen
-                Intent intent = new Intent(Goal.this, Challenges.class);
-                startActivity(intent);
-            }
 
+                for(String goals:selectedItems) {
+
+                   // helper.delete_goals(goals); //remove the user's goals from the database
+                    //Message.message(getActivity().getApplicationContext(),goals);
+                    int i= helper.delete_goals(goals);
+                    if(i <= 0)
+                    {
+                        Message.message(getActivity().getApplicationContext(),"Unable to delete goal");
+
+                    }
+                    else
+                    {
+                        Message.message(getActivity().getApplicationContext(), "Goal deleted");
+
+                    }
+                   // arrayList.remove(goals); //remove the user's goals
+                    adapter.notifyDataSetChanged();
+                }
+
+            }
         });
 
- /*   @Override
-    protected void onRestoreInstanceState(Bundle state){
-        super.onRestoreInstanceState(state);
-        name_array.addAll(state.getStringArrayList("key"));
-        setListAdapter(arrayAdapter);
-        arrayAdapter.notifyDataSetChanged();
+
+        return view;
     }
 
-    @Override
-    protected void onSaveInstanceState(Bundle outState){
-        super.onSaveInstanceState(outState);
-        outState.putStringArrayList("key",name_array);
-    }   */
+
+    //Debug
+  /*  public void viewdata(View view)
+    {
+        String data = helper.getData_Goals();
+        Message.message(getActivity().getApplicationContext(),data);
+    }*/
 
 
-    }
+
 }
