02cb5edb7c66340d42e8979facc955f24a227a91
RaphaelNajera <31861631+RaphaelNajera@users.noreply.github.com>
2018-01-12 01:41:32 -0500

    Updated Challenges.java
    Added the use of firebase to read the task from the firebase database

--- ARJFitTrack/src/main/java/arj/fittrack/Challenges.java
+++ ARJFitTrack/src/main/java/arj/fittrack/Challenges.java
@@ -13,9 +13,12 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -32,12 +35,16 @@ public class Challenges extends Fragment {
     //Declare Arraylist
     ArrayList<String> selectedItems;
     static ArrayList<String> arrayList;
+    static ArrayAdapter<String> adapter;
+
+
+    ListView chl;
 
     //Declare Database
     myDbAdapter helper;
 
     //Firebase
-    DatabaseReference databaseRefGoal;
+    DatabaseReference databaseRefChallenges;
 
 
 
@@ -70,18 +77,7 @@ public class Challenges extends Fragment {
         //Initialize the DatabaseHelper
         helper = new myDbAdapter(getActivity().getApplicationContext());
 
-
-        //create an ArrayList object to store selected items
-        selectedItems = new ArrayList<String>();
-
-
-        //Create an instance of ListView
-        final ListView chl=(ListView) view.findViewById(R.id.checkable_list);
-        //Set multiple selection mode
-        chl.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
-
-
-
+        databaseRefChallenges = FirebaseDatabase.getInstance().getReference("challenges");
 
         //This section will be storing the Challenges and Task in a String
         String[] items = new String[0];
@@ -89,25 +85,46 @@ public class Challenges extends Fragment {
         //Create an ArrayList object to store the challenges and tasks
         arrayList = new ArrayList<>(Arrays.asList(items));
 
+        //Create an instance of ListView
+        chl=(ListView) view.findViewById(R.id.checkable_list);
+        //Set multiple selection mode
+        chl.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+
+
+        //create an ArrayList object to store selected items
+        selectedItems = new ArrayList<String>();
+
+
+
+
+
+
+
+
+
+
+
+
+
         //Gets the user's goals from the database
-        String data = helper.getData_Challenges();
-        String[] test = data.split("\n");
+        //String data = helper.getData_Challenges();
+       // String[] test = data.split("\n");
         //Displays challenges
-        for(String challenges : test) {
-            arrayList.add(challenges);
-        }
+       // for(String challenges : test) {
+        //    arrayList.add(challenges);
+       // }
 
         //supply data items to Checkbox ListView
         //ArrayAdapter<String> aa=new ArrayAdapter<String>(this,R.layout.checkablelist,R.id.txt_title,items);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity().getApplicationContext(), android.R.layout.simple_list_item_multiple_choice, arrayList);
+        //ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity().getApplicationContext(), android.R.layout.simple_list_item_multiple_choice, arrayList);
 
-        chl.setAdapter(adapter);
+       /// chl.setAdapter(adapter);
         chl.setItemChecked(0,true);
 
-        Toast.makeText((getActivity().getApplicationContext()), Integer.toString(chl.getCount()), Toast.LENGTH_LONG).show();
+       // Toast.makeText((getActivity().getApplicationContext()), Integer.toString(chl.getCount()), Toast.LENGTH_LONG).show();
 
 
-        for(int i = 0; i < chl.getCount(); i++){
+        /*for(int i = 0; i < chl.getCount(); i++){
             chl.setItemChecked(i,sharedPreferences.getBoolean(Integer.toString(i),false));
             if(sharedPreferences.getBoolean(Integer.toString(i),false)){
                 chl.setItemChecked(i,true);
@@ -115,7 +132,7 @@ public class Challenges extends Fragment {
             else{
                 chl.setItemChecked(i,false);
             }
-        }
+        }*/
 
 
        // chl.setChe
@@ -129,7 +146,7 @@ public class Challenges extends Fragment {
                 String selectedItem = ((TextView) view).getText().toString();
                 if (selectedItems.contains(selectedItem)){
                     selectedItems.remove(selectedItem); //remove deselected item from the list of selected items
-                editor.putBoolean(Integer.toString(position), false);
+                //editor.putBoolean(Integer.toString(position), false);
 
                 editor.commit();
             }
@@ -138,7 +155,7 @@ public class Challenges extends Fragment {
 
 
                     chl.setItemChecked(position,true);
-                    editor.putBoolean(Integer.toString(position),true);
+                   // editor.putBoolean(Integer.toString(position),true);
 
                     editor.commit();
 
@@ -157,6 +174,33 @@ public class Challenges extends Fragment {
 
     }
 
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        databaseRefChallenges.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                arrayList.clear();
+                //goalidlist.clear();
+                //Retrieve the User's goal and goal id and display it on List of goals
+                for (DataSnapshot goalsnapshot : dataSnapshot.getChildren()) {
+                    challengeslist challenges = goalsnapshot.getValue(challengeslist.class);
+                    arrayList.add(challenges.getchallenge());
+                   // goalidlist.add(goal.getgoalID());
+                }
+                adapter = new ArrayAdapter<String>(getActivity().getApplicationContext(), android.R.layout.simple_list_item_multiple_choice, arrayList);
+                chl.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
 
 /*
     public void onStart(){
@@ -181,5 +225,8 @@ public class Challenges extends Fragment {
         Message.message((getActivity().getApplicationContext()),data);
     }
 */
+
+
+
 
 }
